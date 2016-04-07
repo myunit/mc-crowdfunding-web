4057f52ff72e3b5ea1f3ecf8a3bf8ba56849469d
joneqian <qian.qing@aliyun.com>
2016-04-07 23:01:56 +0800

    api

--- public/js/controller/invest-ctrl.js
+++ public/js/controller/invest-ctrl.js
@@ -182,6 +182,24 @@ require(['Vue', 'Utils'],
                         if (data.count === 1) {
                             vm.funding = Utils.clone(data.funding);
                             vm.imgList = data.img.slice();
+                            Vue.nextTick(function () {
+                                $('#my-carousel').carousel();
+                                $('[id^=carousel-selector-]').click( function(){
+                                    var id_selector = $(this).attr("id");
+                                    var id = id_selector.substr(id_selector.length -1);
+                                    id = parseInt(id);
+                                    $('#myCarousel').carousel(id);
+                                    $('[id^=carousel-selector-]').removeClass('selected');
+                                    $(this).addClass('selected');
+                                });
+
+                                $('#myCarousel').on('slid', function (e) {
+                                    var id = $('.item.active').data('slide-number');
+                                    id = parseInt(id);
+                                    $('[id^=carousel-selector-]').removeClass('selected');
+                                    $('[id=carousel-selector-'+id+']').addClass('selected');
+                                });
+                            });
                         }
                     }
                 });
